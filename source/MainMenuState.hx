82064262d7cd49097bff2ad0520c634a72ac93bb
Fioon <135500399+Fioon@users.noreply.github.com>
2024-07-09 12:03:19 +0800

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -47,6 +47,8 @@ class MainMenuState extends MusicBeatState
 
 	override function create()
 	{
+		SUtil.applicationAlert('create', 'c');
+		
 		#if windows
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("In the Menus", null);
