ee726e3b7fdd056322a82eaccdd054c6d8bd196f
Fioon <135500399+Fioon@users.noreply.github.com>
2024-07-09 18:15:07 +0800

    Update OptionsMenu.hx

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -23,13 +23,17 @@ class OptionsMenu extends MusicBeatState
 
 	var options:Array<OptionCatagory> = [
 		new OptionCatagory("Gameplay", [
+			#if mobileC
+			new CustomControls("Edit a mobile controls..."),
+			#end
 			new DFJKOption(controls),
 			new DownscrollOption("Change the layout of the strumline."),
 			new GhostTapOption("Ghost Tapping is when you tap a direction and it doesn't give you a miss."),
-			new Judgement("Customize your Hit Timings (LEFT or RIGHT)"),
-			#if desktop
-			new FPSCapOption("Cap your FPS"),
+			#if mobileC
+			new FastValue("Switch speed of changing value in bottom. (e.g. offset)"),
 			#end
+			new Judgement("Customize your Hit Timings (LEFT or RIGHT)"),
+			new FPSCapOption("Cap your FPS"),
 			new ScrollSpeedOption("Change your scroll speed (1 = Chart dependent)"),
 			new AccuracyDOption("Change how accuracy is calculated. (Accurate = Simple, Complex = Milisecond Based)"),
 			new ResetButtonOption("Toggle pressing R to gameover."),
@@ -37,23 +41,18 @@ class OptionsMenu extends MusicBeatState
 			new CustomizeGameplay("Drag'n'Drop Gameplay Modules around to your preference")
 		]),
 		new OptionCatagory("Appearance", [
-			#if desktop
 			new DistractionsAndEffectsOption("Toggle stage distractions that can hinder your gameplay."),
 			new RainbowFPSOption("Make the FPS Counter Rainbow"),
 			new AccuracyOption("Display accuracy information."),
 			new NPSDisplayOption("Shows your current Notes Per Second."),
 			new SongPositionOption("Show the songs current position (as a bar)"),
 			new CpuStrums("CPU's strumline lights up when a note hits it."),
-			#else
-			new DistractionsAndEffectsOption("Toggle stage distractions that can hinder your gameplay.")
-			#end
+			//new DistractionsAndEffectsOption("Toggle stage distractions that can hinder your gameplay.")
 		]),
 		
 		new OptionCatagory("Misc", [
-			#if desktop
 			new FPSOption("Toggle the FPS Counter"),
 			new ReplayOption("View replays"),
-			#end
 			new FlashingLightsOption("Toggle flashing lights that can cause epileptic seizures and strain."),
 			new WatermarkOption("Enable and disable all watermarks from the engine."),
 			new BotPlay("Showcase your charts and mods with autoplay.")
@@ -106,6 +105,10 @@ class OptionsMenu extends MusicBeatState
 		FlxTween.tween(versionShit,{y: FlxG.height - 18},2,{ease: FlxEase.elasticInOut});
 		FlxTween.tween(blackBorder,{y: FlxG.height - 18},2, {ease: FlxEase.elasticInOut});
 
+		#if mobileC
+		addVirtualPad(FULL, A_B);
+		#end
+			
 		super.create();
 	}
 
@@ -167,6 +170,15 @@ class OptionsMenu extends MusicBeatState
 						else if (FlxG.keys.justPressed.LEFT)
 							FlxG.save.data.offset -= 0.1;
 					}
+					#if mobileC
+					else if (FlxG.save.data.fastValue == 2)
+					{
+						if (controls.RIGHT)
+							FlxG.save.data.offset += 1;
+						else if (controls.LEFT)
+							FlxG.save.data.offset -= 1;
+					}
+					#end
 					else if (FlxG.keys.pressed.RIGHT)
 						FlxG.save.data.offset += 0.1;
 					else if (FlxG.keys.pressed.LEFT)
@@ -188,6 +200,15 @@ class OptionsMenu extends MusicBeatState
 						else if (FlxG.keys.justPressed.LEFT)
 							FlxG.save.data.offset -= 0.1;
 					}
+				#if mobileC
+				else if (FlxG.save.data.fastValue == 2)
+				{
+					if (controls.RIGHT)
+						FlxG.save.data.offset += 1;
+					else if (controls.LEFT)
+						FlxG.save.data.offset -= 1;
+				}
+				#end
 					else if (FlxG.keys.pressed.RIGHT)
 						FlxG.save.data.offset += 0.1;
 					else if (FlxG.keys.pressed.LEFT)
@@ -195,8 +216,10 @@ class OptionsMenu extends MusicBeatState
 			}
 		
 
+		        #if !mobileC
 			if (controls.RESET)
 					FlxG.save.data.offset = 0;
+		        #end
 
 			if (controls.ACCEPT)
 			{
