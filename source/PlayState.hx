5dedef9d4b965d64467a369794c09f740924271c
Fioon <135500399+Fioon@users.noreply.github.com>
2024-07-11 15:16:37 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2502,6 +2502,7 @@ class PlayState extends MusicBeatState
 	}
 	function endSong():Void
 	{
+		#if windows
 		if (!loadRep)
 			rep.SaveReplay(saveNotes);
 		else
@@ -2510,6 +2511,7 @@ class PlayState extends MusicBeatState
 			FlxG.save.data.scrollSpeed = 1;
 			FlxG.save.data.downscroll = false;
 		}
+		#end
 
 		if (FlxG.save.data.fpsCap > 290)
 			(cast (Lib.current.getChildAt(0), Main)).setFPSCap(290);
