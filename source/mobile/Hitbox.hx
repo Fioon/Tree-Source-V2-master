c4cba9f6522d7071863807d3628e363108c32937
Fioon <135500399+Fioon@users.noreply.github.com>
2024-07-28 15:30:45 +0800

    Update Hitbox.hx

--- source/mobile/Hitbox.hx
+++ source/mobile/Hitbox.hx
@@ -10,12 +10,14 @@ import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxPoint;
 import flixel.system.FlxAssets;
 import flixel.util.FlxDestroyUtil;
-import flixel.ui.FlxButton;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.frames.FlxFrame;
 import flixel.ui.FlxVirtualPad;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
+import openfl.display.BitmapData;
+import openfl.display.Shape;
+import mobile.flixel.FlxButton;
 
 // copyed from flxvirtualpad
 class Hitbox extends FlxSpriteGroup
@@ -36,7 +38,7 @@ class Hitbox extends FlxSpriteGroup
         super();
 
         /*if (widghtScreen == null)
-            widghtScreen = FlxG.width;*/
+            widghtScreen = FlxG.width;
 
         sizex = widghtScreen != null ? Std.int(widghtScreen / 4) : 320;
 
@@ -58,9 +60,56 @@ class Hitbox extends FlxSpriteGroup
 
         hitbox.add(add(buttonUp = createhitbox(sizex * 2, "up")));
 
-        hitbox.add(add(buttonRight = createhitbox(sizex * 3, "right")));
-    }
+        hitbox.add(add(buttonRight = createhitbox(sizex * 3, "right")));*/
 
+
+        
+        add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 1), 0xFF00FF));
+		add(buttonDown = createHint(FlxG.width / 4, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 1), 0x00FFFF));
+		add(buttonUp = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 1), 0x00FF00));
+		add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 1), 0xFF0000));
+        
+    }
+    
+    private function createHintGraphic(Width:Int, Height:Int, Color:Int = 0xFFFFFF):BitmapData
+	{
+		var shape:Shape = new Shape();
+		shape.graphics.beginFill(Color);
+		shape.graphics.lineStyle(10, Color, 1);
+		shape.graphics.drawRect(0, 0, Width, Height);
+		shape.graphics.endFill();
+
+		var bitmap:BitmapData = new BitmapData(Width, Height, true, 0);
+		bitmap.draw(shape);
+		return bitmap;
+	}
+
+	private function createHint(X:Float, Y:Float, Width:Int, Height:Int, Color:Int = 0xFFFFFF):FlxButton
+	{
+		var hint:FlxButton = new FlxButton(X, Y);
+		hint.loadGraphic(createHintGraphic(Width, Height, Color));
+		hint.solid = false;
+		hint.immovable = true;
+		hint.scrollFactor.set();
+		hint.alpha = 0.00001;
+		hint.onDown.callback = hint.onOver.callback = function()
+		{
+			if (hint.alpha != ClientPrefs.hitboxalpha)
+				hint.alpha = ClientPrefs.hitboxalpha;
+		}
+		hint.onUp.callback = hint.onOut.callback = function()
+		{
+			if (hint.alpha != 0.00001)
+				hint.alpha = 0.00001;
+		}
+		#if FLX_DEBUG
+		hint.ignoreDrawDebug = true;
+		#end
+		return hint;
+	}
+
+
+    
     public function createhitbox(X:Float, framestring:String) {
         var button = new FlxButton(X, 0);
         var frames = FlxAtlasFrames.fromSparrow('assets/shared/images/hitbox/hitbox.png', 'assets/shared/images/hitbox/hitbox.xml');
